d380cd2b85f617150b4a2c68d0bc60d304887a94
RobinCoco <robinlb@live.fr>
2026-06-27 12:48:34 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,4 @@
-
+import { createRoot } from 'react-dom/client'
 import CocoProno from './CocoProno.jsx'
-import ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<CocoProno />)
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode><App /></StrictMode>
-)
+createRoot(document.getElementById('root')).render(<CocoProno />)
